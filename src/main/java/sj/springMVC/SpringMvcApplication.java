ef3520bfd09f12c26f26d2113dfa26444a018e11
seongju <lsj40413@gmail.com>
2023-05-15 22:15:38 +0900

    feat: init SpringMVC

---
+++ src/main/java/sj/springMVC/SpringMvcApplication.java
@@ -0,0 +1,15 @@
+package sj.springMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan // 서블릿 자동등록
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+}
